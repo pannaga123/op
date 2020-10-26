97f3c4ec27008ffc4742a77a7d5d3993b034bc72
pannaga123 <47821974+pannaga123@users.noreply.github.com>
2020-10-26 13:41:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>my different name</h1>
+    <ul>
+      <li>pannaga</li>
+      <li>pannaga shastri</li>
+      <li>yo panna</li>
+      <li>dfsf</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
